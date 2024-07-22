1fe11b1eebc6314f5f2ec1bd83b89ec479b79d9a
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-22 16:13:46 -0600

    feat: Make http request to obtain locations

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchResultsComponent } from "../search-results/search-results.component";
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-search-bar',
@@ -10,13 +11,15 @@ import { SearchResultsComponent } from "../search-results/search-results.compone
 })
 export class SearchBarComponent {
 
+  private placesService = inject(PlacesService);
+
   private debounceTimer?:NodeJS.Timeout;
 
   onQueryChanged(query:string = '') {
 
     if(this.debounceTimer) clearTimeout(this.debounceTimer);
     this.debounceTimer = setTimeout(() => {
-
+      this.placesService.getPlacesByQuery(query);
     },350);
 
   }
--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +9,14 @@ export class PlacesService {
 
   public userLocation?:[number,number];
 
+  public isLoadingPlaces:boolean = false;
+  public places:Feature[]=[];
+
   get isUserLocationReady():boolean{
     return !!this.userLocation;
   }
 
-  constructor() {
+  constructor( private http:HttpClient) {
     this.getUserLocation();
   }
 
@@ -30,6 +35,18 @@ export class PlacesService {
         }
       );
     });
+  }
+
+  getPlacesByQuery( query:String = '') {
+
+    this.isLoadingPlaces = true;
+
+    this.http.get<PlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?country=mx&proximity=-103.3774625890349%2C20.643222603680016&language=es&access_token=pk.eyJ1Ijoid2FjaGltaXJvIiwiYSI6ImNseWoxNjRoZjBsODYycW9tZDZqNGcxdXkifQ.WhbOpeFml-jIjGjTTTJFzw`)
+      .subscribe( resp => {
+        console.log(resp.features);
+        this.isLoadingPlaces = false;
+        this.places = resp.features;
+      });
   }
 
 }

4848685166e843d141ec62f108a52d443ed9fd3d
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-22 13:39:23 -0600

    feat: Floating button and angular logo

---
+++ src/app/maps/components/btn-my-location/btn-my-location.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-my-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-my-location.component.html',
+  styleUrl: './btn-my-location.component.css'
+})
+export class BtnMyLocationComponent {
+
+  goToMyLocation() {
+    throw new Error('Method not implemented.');
+  }
+
+}
--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -3,11 +3,13 @@ import { PlacesService } from '../../services';
 import { MapViewComponent } from "../../components/map-view/map-view.component";
 import { LoadingComponent } from '../../components/loading/loading.component';
 import { CommonModule } from '@angular/common';
+import { AngularLogoComponent } from "../../components/angular-logo/angular-logo.component";
+import { BtnMyLocationComponent } from "../../components/btn-my-location/btn-my-location.component";
 
 @Component({
   selector: 'screens-map-screen',
   standalone: true,
-  imports: [CommonModule,MapViewComponent,LoadingComponent],
+  imports: [CommonModule, MapViewComponent, LoadingComponent, AngularLogoComponent, BtnMyLocationComponent],
   templateUrl: './map-screen.component.html',
   styleUrl: './map-screen.component.css'
 })

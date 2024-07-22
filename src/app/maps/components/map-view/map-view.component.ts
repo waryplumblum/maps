7a8f00cb391f23008f38c406214d95b0f6b27299
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-22 13:30:51 -0600

    feat: Markers and Popups

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { PlacesService } from '../../services/places.service';
-import { Map } from 'mapbox-gl';
+import { Map, Marker, Popup } from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-view',
@@ -16,13 +16,27 @@ export class MapViewComponent implements AfterViewInit {
   @ViewChild('mapDiv') mapDivElement!:ElementRef;
 
   ngAfterViewInit(): void {
+
     if(!this.placesService.userLocation) throw Error('No hay placesService.useLocation');
+
     const map = new Map({
       container: this.mapDivElement.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: this.placesService.userLocation, // starting position [lng, lat]
       zoom: 14, // starting zoom
     });
+
+    const popup = new Popup()
+      .setHTML(`
+        <h6>Aquí estoy.</h6>
+        <span>Estoy en este lugar del mundo.</span>
+      `);
+
+    new Marker({ color: 'red' })
+      .setLngLat(this.placesService.userLocation)
+      .setPopup(popup)
+      .addTo(map)
+
   }
 
 }

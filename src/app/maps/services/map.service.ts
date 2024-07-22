4879a635ca8604efb6ed123f8056631bceec3a7d
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-22 17:42:30 -0600

    feat: Add markers on places from search bar

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
-import { LngLatLike, Map } from 'mapbox-gl';
+import { LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { Feature } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  private map?: Map;
+  private map    ?: Map;
+  private markers :Marker[]=[];
 
   get isMapReady(){
     return !!this.map;
@@ -20,6 +22,28 @@ export class MapService {
     if(!this.isMapReady) throw Error('El mapa no esta inicializado');
 
     this.map?.flyTo({ zoom:14, center: coords});
+  }
+
+  createMarkersFromPlaces(places:Feature[]){
+
+    if(!this.map) throw Error('Mapa no inicializado.');
+    this.markers.forEach(marker => marker.remove());
+    const newMarkers = [];
+    for( const place of places){
+      const [lng,lat] = place.center;
+      const popup = new Popup()
+        .setHTML(`
+          <h6>${place.text}</h6>
+          <span>${place.place_name}</span>
+        `);
+      const newMarker = new Marker()
+        .setLngLat([lng,lat])
+        .setPopup(popup)
+        .addTo(this.map);
+      newMarkers.push(newMarker);
+    }
+    this.markers = newMarkers;
+
   }
 
 }
--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Feature, PlacesResponse } from '../interfaces/places';
 import { PlacesApiClient } from '../api/placesApiClient';
+import { MapService } from './map.service';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,10 @@ export class PlacesService {
     return !!this.userLocation;
   }
 
-  constructor( private placesApi:PlacesApiClient) {
+  constructor(
+    private placesApi:PlacesApiClient,
+    private mapService:MapService,
+  ) {
     this.getUserLocation();
   }
 
@@ -59,6 +62,7 @@ export class PlacesService {
         // console.log(resp.features);
         this.isLoadingPlaces = false;
         this.places = resp.features;
+        this.mapService.createMarkersFromPlaces(this.places);
       });
   }
 
